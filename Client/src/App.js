7def0a3fa2d4cc9d2cf9cdeb8180b3300b0971ec
Kostadin Lambov <valchak@abv.bg>
2019-02-05 04:02:18 +0100

    Client initial commit

---
+++ Client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     <h1 className="App-center">Hello from Social Network!</h1>
+    )
+  }
+}
+
+export default App;
