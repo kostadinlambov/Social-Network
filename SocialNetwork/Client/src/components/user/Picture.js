ed272a3248187d2f50e1da426d483f9c0e34b99f
Kostadin Lambov <valchak@abv.bg>
2019-03-09 07:35:53 +0100

    Client: Added pictures delete button styles

--- SocialNetwork/Client/src/components/user/Picture.js
+++ SocialNetwork/Client/src/components/user/Picture.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import {userService} from '../../infrastructure'
+import { userService } from '../../infrastructure'
 
 
 const Picture = (props) => {
-   const imageClass =  userService.getImageSize(props.imageUrl);
+    const imageClass = userService.getImageSize(props.imageUrl);
 
     return (
 
         <li>
-            <article className="card">
-                <div className="media">
-                    <img className={imageClass} src={props.imageUrl} alt="pic1" />
-                </div>
-                {/* <div className="content">
+            <div id="container">
+                <article className="card " id="contauner">
+                    <div className="media">
+                        <img className={imageClass} src={props.imageUrl} alt="pic1" />
+                    </div>
+                    {/* <div className="content">
                                 <h3 className="card-title">Philip J. Fry</h3>
                                 <p></p>
                 </div> */}
-            </article>
+                    {/* <div className="fbPhotoCurationControl uiButtonGroup far fa-trash-alt" id="delete-button"></div> */}
+                    <button className="">
+                        <div className="fbPhotoCurationControl inner uiButtonGroup " ><i class="far fa-trash-alt "></i></div>
+                    </button>
+                </article>
+            </div>
+
+
+
         </li>
     )
 }
